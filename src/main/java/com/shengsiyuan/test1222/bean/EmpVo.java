60fb4b737445513b4d98baec7eb58de03d16df45
glb_hafjk <glb_hafjk@qq.com>
2019-12-22 13:40:52 +0800

    456

--- src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
+++ src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
@@ -2,4 +2,12 @@ package com.shengsiyuan.test1222.bean;
 
 public class EmpVo {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
