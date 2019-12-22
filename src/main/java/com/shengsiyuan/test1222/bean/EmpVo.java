45e24d6ea97d25c1b1fcea8ff3e3442f6819f3f9
glb_hafjk <glb_hafjk@qq.com>
2019-12-22 14:14:45 +0800

    56

--- src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
+++ src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
@@ -2,6 +2,15 @@ package com.shengsiyuan.test1222.bean;
 
 public class EmpVo {
     private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
