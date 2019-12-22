20bf42352bbcafb9338c6c7749125f3394433982
glb_hafjk <glb_hafjk@qq.com>
2019-12-22 14:16:08 +0800

    231

--- src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
+++ src/main/java/com/shengsiyuan/test1222/bean/EmpVo.java
@@ -2,6 +2,15 @@ package com.shengsiyuan.test1222.bean;
 
 public class EmpVo {
     private String name;
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
